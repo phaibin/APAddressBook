6e658d20bc62fcada37c8853e6c03643edb2f917
Leon <phaibin@gmail.com>
2014-03-08 15:34:30 +0800

    change sort rule

--- AddressBook/AddressBook/ListViewController.m
+++ AddressBook/AddressBook/ListViewController.m
@@ -64,8 +64,17 @@
     __weak __typeof(self) weakSelf = self;
     addressBook.fieldsMask = APContactFieldAll;
     addressBook.sortDescriptors = @[
-    [NSSortDescriptor sortDescriptorWithKey:@"firstName" ascending:YES],
-    [NSSortDescriptor sortDescriptorWithKey:@"lastName" ascending:YES]];
+                                    [NSSortDescriptor sortDescriptorWithKey:@"fullName" ascending:YES comparator:^NSComparisonResult(id obj1, id obj2) {
+                                        NSString *string1 = (NSString *)obj1;
+                                        NSString *string2 = (NSString *)obj2;
+                                        static NSStringCompareOptions comparisonOptions = NSNumericSearch;
+                                        NSLocale *locale=[[NSLocale alloc] initWithLocaleIdentifier:@"zh_CN"];
+                                        NSRange string1Range = NSMakeRange(0, [string1 length]);
+                                        return [string1 compare:string2
+                                                        options:comparisonOptions
+                                                          range:string1Range
+                                                         locale:(NSLocale *)locale];
+                                    }]];
     addressBook.filterBlock = ^BOOL(APContact *contact)
     {
         return contact.phones.count > 0;
